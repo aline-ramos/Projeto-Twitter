e83f615da894d0fa41f5952f90268fc3e5923d46
aline-ramos <alineramos.costa@gmail.com>
2019-02-21 14:57:04 -0300

    Projeto concluído

--- novo.js
+++ novo.js
@@ -1,39 +1,41 @@
-const lengthvalor= 140;
+const lengthvalor = 140;
 
-let tweetarMensagem=document.getElementById("tweetarMensagem");
-let tweetBotao=document.getElementById("tweetBotao");
-let listTweet=document.getElementById("tweetList");
-let counter= document.getElementById("counter");
+let tweetarMensagem = document.getElementById("tweetarMensagem");
+let tweetBotao = document.getElementById("tweetBotao");
+let listTweet = document.getElementById("tweetList");
+let counter = document.getElementById("counter");
 
 tweetBotao.addEventListener("click", tweetar);
-
-
-function tweetar(event){
-let mensagemTweet= document.createElement("li");
-mensagemTweet.textContent=tweetarMensagem.value;
-tweetList.appendChild(mensagemTweet);
-tweetarMensagem.value="";
-tweetBotao.setAttribute("disabled", "");
-
+function tweetar(event) {
+    let mensagemTweet = document.createElement("li");
+    mensagemTweet.textContent = tweetarMensagem.value;
+    tweetList.appendChild(mensagemTweet);
+    tweetarMensagem.value = "";
+    tweetBotao.setAttribute("disabled", "");
 }
-
 tweetarMensagem.addEventListener("keyup", counterUp)
-
-function counterUp(event){
-let counterValue = lengthvalor - tweetarMensagem.value.length;
-counter.textContent=counterValue;
-tweetBotao.removeAttribute("disabled");
-if (counterValue === lengthvalor || counterValue < 0) {
-    tweetBotao.setAttribute("disabled", "");    
-} else{
-    
-
+function counterUp(event) {
+    let counterValue = lengthvalor - tweetarMensagem.value.length;
+    counter.textContent = counterValue;
+    tweetBotao.removeAttribute("disabled");
+    if (counterValue === lengthvalor || counterValue < 0) {
+        tweetBotao.setAttribute("disabled", "");
+    } else {
 }
-
-
+    if (counterValue < 20 && counterValue >= 10) {
+        counter.setAttribute("class", "counter-blue");
+} else if (counterValue < 10) {
+        counter.setAttribute("class", "counter-red");
+} else {
+        counter.setAttribute("class", "counter-white");
+    }
 }
-
-
+function autoResize() {
+    objTextArea = document.getElementById('tweetarMensagem');
+    while (objTextArea.scrollHeight > objTextArea.offsetHeight) {
+        objTextArea.rows += 1;
+    }
+}
 
 
 
